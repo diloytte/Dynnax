16b1a01bf547cbf46694743c38a8fdd0a33d55be
diloytte <mail@diloytte.com>
2025-04-29 12:24:25 +0200

    quick fix

--- shared/src/pf/mod.rs
+++ shared/src/pf/mod.rs
@@ -75,6 +75,7 @@ pub async fn buy_ca(
             let dex_address = fetch_base_token_address_from_dex(ca, http_client).await;
             if dex_address.is_err() {
                 println!("Error from DEX. {:?}", error);
+                return Ok(());
             }
             match send_trade(
                 &TradeRequest::Buy(&TradeRequestBuy {
